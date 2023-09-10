3e9bce3c358fc4b029b28060a6eaf48699f90263
Giankrp <gianruiz2003@gmail.com>
2023-09-10 15:38:14 -0500

    mux commit

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Giankrp/firstBack/db"
 	"github.com/Giankrp/firstBack/models"
 	"github.com/Giankrp/firstBack/routes"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -15,6 +16,9 @@ func main() {
 	db.DB.AutoMigrate(models.User{})
 	db.DB.AutoMigrate(models.Task{})
 	r := mux.NewRouter()
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 	r.HandleFunc("/", routes.HomeHandler)
 	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
 	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
@@ -28,5 +32,5 @@ func main() {
 	r.HandleFunc("/tasks", routes.CreateTaskHandler).Methods("POST")
 	r.HandleFunc("/tasks/{id}", routes.DeleteTaskHandler).Methods("DELETE")
 
-	http.ListenAndServe(":8000", r)
+	http.ListenAndServe(":8000", handlers.CORS(headersOk, originsOk, methodsOk)(r))
 }
